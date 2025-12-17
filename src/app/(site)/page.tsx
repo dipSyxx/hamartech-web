88c8c5ee60a382ecd5c705d9aeac174d77152d36
dipSyxx <linetsky.yura@gmail.com>
2025-12-17 09:39:22 +0100

    Remove commented-out code for global gradient blur circles in Home component to clean up the codebase.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -76,7 +76,6 @@ export default function Home() {
 
   return (
     <div className="relative overflow-hidden">
-      {/* Глобальні градієнтові blur-кружки */}
       <BackgroundGlows />
 
       {/* HERO */}
